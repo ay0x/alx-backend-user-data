731f4b671127ceae3736a5a6afc26578029b3418
ay0x <85720179+ay0x@users.noreply.github.com>
2024-05-28 17:10:46 +0200

    Added flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, request, jsonify, abort, redirect, url_for
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """
+    Return json respomse
+    {"message": "Bienvenue"}
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
